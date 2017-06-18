44310d6cb0a9f6141c32666b2d4d49f09c20683e
Nathan Fuchs <fuchsnj@gmail.com>
2017-06-18 13:13:48 -0400

    add comparison bytecode

--- src/module/sections/code_section/bytecode/mod.rs
+++ src/module/sections/code_section/bytecode/mod.rs
@@ -4,6 +4,7 @@ pub mod parametric;
 pub mod variable;
 pub mod memory;
 pub mod constant;
+pub mod comparison;
 
 use std::io::Write;
 use std::io;
@@ -67,6 +68,44 @@ pub enum AnyBytecode {
 	F32Constant(constant::F32Constant),
 	F64Constant(constant::F64Constant),
 
+	I32EqualToZero(comparison::I32EqualToZero),
+	I32Equal(comparison::I32Equal),
+	I32NotEqual(comparison::I32NotEqual),
+	I32LessThanSigned(comparison::I32LessThanSigned),
+	I32LessThanUnsigned(comparison::I32LessOrEqualUnsigned),
+	I32GreaterThanSigned(comparison::I32GreaterThanSigned),
+	I32GreaterThanUnsigned(comparison::I32GreaterThanUnsigned),
+	I32LessOrEqualSigned(comparison::I32LessOrEqualSigned),
+	I32LessOrEqualUnsigned(comparison::I32LessOrEqualUnsigned),
+	I32GreaterOrEqualSigned(comparison::I32GreaterOrEqualSigned),
+	I32GreaterOrEqualUnsigned(comparison::I32GreaterThanUnsigned),
+
+	I64EqualToZero(comparison::I64EqualToZero),
+	I64Equal(comparison::I64Equal),
+	I64NotEqual(comparison::I64NotEqual),
+	I64LessThanSigned(comparison::I64LessThanSigned),
+	I64LessThanUnsigned(comparison::I64LessThanUnsigned),
+	I64GreaterThanSigned(comparison::I64GreaterThanSigned),
+	I64GreaterThanUnsigned(comparison::I64GreaterThanUnsigned),
+	I64LessOrEqualSigned(comparison::I64LessOrEqualSigned),
+	I64LessOrEqualUnsigned(comparison::I64LessOrEqualUnsigned),
+	I64GreaterOrEqualSigned(comparison::I64GreaterOrEqualSigned),
+	I64GreaterOrEqualUnsigned(comparison::I64GreaterOrEqualUnsigned),
+
+	F32Equal(comparison::F32Equal),
+	F32NotEqual(comparison::F32NotEqual),
+	F32LessThan(comparison::F32LessThan),
+	F32GreaterThan(comparison::F32GreaterThan),
+	F32LessOrEqual(comparison::F32LessOrEqual),
+	F32GreaterOrEqual(comparison::F32GreaterOrEqual),
+
+	F64Equal(comparison::F64Equal),
+	F64NotEqual(comparison::F64NotEqual),
+	F64LessThan(comparison::F64LessThan),
+	F64GreaterThan(comparison::F64GreaterThan),
+	F64LessOrEqual(comparison::F64LessOrEqual),
+	F64GreaterOrEqual(comparison::F32GreaterOrEqual),
+
 }
 
 impl Bytecode for AnyBytecode {
@@ -125,6 +164,44 @@ impl Bytecode for AnyBytecode {
 			AnyBytecode::I64Constant(ref x) => x.compile(out),
 			AnyBytecode::F32Constant(ref x) => x.compile(out),
 			AnyBytecode::F64Constant(ref x) => x.compile(out),
+
+			AnyBytecode::I32EqualToZero(ref x) => x.compile(out),
+			AnyBytecode::I32Equal(ref x) => x.compile(out),
+			AnyBytecode::I32NotEqual(ref x) => x.compile(out),
+			AnyBytecode::I32LessThanSigned(ref x) => x.compile(out),
+			AnyBytecode::I32LessThanUnsigned(ref x) => x.compile(out),
+			AnyBytecode::I32GreaterThanSigned(ref x) => x.compile(out),
+			AnyBytecode::I32GreaterThanUnsigned(ref x) => x.compile(out),
+			AnyBytecode::I32LessOrEqualSigned(ref x) => x.compile(out),
+			AnyBytecode::I32LessOrEqualUnsigned(ref x) => x.compile(out),
+			AnyBytecode::I32GreaterOrEqualSigned(ref x) => x.compile(out),
+			AnyBytecode::I32GreaterOrEqualUnsigned(ref x) => x.compile(out),
+
+			AnyBytecode::I64EqualToZero(ref x) => x.compile(out),
+			AnyBytecode::I64Equal(ref x) => x.compile(out),
+			AnyBytecode::I64NotEqual(ref x) => x.compile(out),
+			AnyBytecode::I64LessThanSigned(ref x) => x.compile(out),
+			AnyBytecode::I64LessThanUnsigned(ref x) => x.compile(out),
+			AnyBytecode::I64GreaterThanSigned(ref x) => x.compile(out),
+			AnyBytecode::I64GreaterThanUnsigned(ref x) => x.compile(out),
+			AnyBytecode::I64LessOrEqualSigned(ref x) => x.compile(out),
+			AnyBytecode::I64LessOrEqualUnsigned(ref x) => x.compile(out),
+			AnyBytecode::I64GreaterOrEqualSigned(ref x) => x.compile(out),
+			AnyBytecode::I64GreaterOrEqualUnsigned(ref x) => x.compile(out),
+
+			AnyBytecode::F32Equal(ref x) => x.compile(out),
+			AnyBytecode::F32NotEqual(ref x) => x.compile(out),
+			AnyBytecode::F32LessThan(ref x) => x.compile(out),
+			AnyBytecode::F32GreaterThan(ref x) => x.compile(out),
+			AnyBytecode::F32LessOrEqual(ref x) => x.compile(out),
+			AnyBytecode::F32GreaterOrEqual(ref x) => x.compile(out),
+
+			AnyBytecode::F64Equal(ref x) => x.compile(out),
+			AnyBytecode::F64NotEqual(ref x) => x.compile(out),
+			AnyBytecode::F64LessThan(ref x) => x.compile(out),
+			AnyBytecode::F64GreaterThan(ref x) => x.compile(out),
+			AnyBytecode::F64LessOrEqual(ref x) => x.compile(out),
+			AnyBytecode::F64GreaterOrEqual(ref x) => x.compile(out),
 		}
 	}
 }
